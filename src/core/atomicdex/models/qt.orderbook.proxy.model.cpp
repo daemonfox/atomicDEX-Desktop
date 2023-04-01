f63d740a63e3c466f507a800ed855c97515ec33d
cipig <cipianon@gmail.com>
2023-04-01 22:03:50 +0000

    no ratelimit

--- src/core/atomicdex/models/qt.orderbook.proxy.model.cpp
+++ src/core/atomicdex/models/qt.orderbook.proxy.model.cpp
@@ -167,7 +167,7 @@ namespace atomic_dex
                 {
                     return false;
                 }
-                if (is_cex_id_available && (rates > 100 || fiat_price <= 0 || ((safe_float(volume) < limit) && coin_info.coin_type != CoinType::SmartChain)))
+                if (is_cex_id_available && (fiat_price <= 0 || ((safe_float(volume) < limit) && coin_info.coin_type != CoinType::SmartChain)))
                 {
                     return false;
                 }
